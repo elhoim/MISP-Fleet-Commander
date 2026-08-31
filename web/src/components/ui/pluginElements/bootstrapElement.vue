2c9b9ade7af497ace8e03a68792a5d57db5ee796
elhoim <elhoim@gmail.com>
2026-08-31 17:51:05 +0000

    fix: allowlist plugin-controlled bootstrap component and props

--- web/src/components/ui/pluginElements/bootstrapElement.vue
+++ web/src/components/ui/pluginElements/bootstrapElement.vue
@@ -1,10 +1,17 @@
 <template>
     <div>
-        <component :is="bsComponentName" v-bind="data">{{ getText }}</component>
+        <component v-if="isAllowedComponent" :is="bsComponentName" v-bind="safeData">{{ getText }}</component>
+        <span v-else>{{ getText }}</span>
     </div>
 </template>
 
 <script>
+// Plugin responses are not trusted: only these presentational components and
+// props may be rendered. Anything else (`b-link href="javascript:..."`, raw
+// `onclick` attributes, ...) is dropped.
+const ALLOWED_COMPONENTS = ['badge', 'alert', 'spinner', 'progress']
+const ALLOWED_PROPS = ['variant', 'title', 'pill', 'size', 'label', 'value', 'max', 'show', 'striped', 'animated', 'small', 'type']
+
 export default {
     name: 'PluginElementBootstrapElement',
     props: {
@@ -20,8 +27,16 @@ export default {
         },
     },
     computed: {
+        isAllowedComponent() {
+            return ALLOWED_COMPONENTS.includes(this.data.component)
+        },
         bsComponentName() {
             return `b-${this.data.component}`
+        },
+        safeData() {
+            return Object.fromEntries(
+                Object.entries(this.data).filter(([key]) => ALLOWED_PROPS.includes(key))
+            )
         },
         getText() {
             return this.data.text ?? ''
