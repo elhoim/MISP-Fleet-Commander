5d101fa67b73bb2c82e972515fb46f1d43fe995e
elhoim <elhoim@gmail.com>
2026-08-31 16:40:43 +0000

    fix: re-enable Save button after discovered-server submission

--- web/src/views/servers/DiscoverServers.vue
+++ web/src/views/servers/DiscoverServers.vue
@@ -129,7 +129,7 @@ export default {
                     })
                 })
                 .finally(() => {
-                    this.postInProgress = true
+                    this.postInProgress = false
                 })
         },
         discoverServer() {
